a3a623a0f31a7a4329cdb55728ba05744a8c2c60
Ronaldo Mariano <ronaldowebdesing@hotmail.com>
2020-11-30 18:57:25 -0300

    update: criacao dos botoes e style

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-let efect = document.querySelector('.efect')
+
+let efect = document.querySelector('#efect')
+
 let efect1 = document.querySelector('.efect_one')
 let efect2 = document.querySelector('.efect_two')
 let efect3 = document.querySelector('.efect_three')
 let efect4 = document.querySelector('.efect_four')
 
-efect1.addEventListener('click', ()=>{
-    efect.classList.toggle("cicle");
+efect1.addEventListener('click', ()=>{ 
+    efect.classList.toggle('position_trapezio');
 });
-console.log(efect1)
+efect2.addEventListener('click', ()=>{
+    efect.classList.toggle('position_cicle');
+});
+efect3.addEventListener('click', ()=>{
+    efect.classList.toggle('position_pentagono');
+});
+efect4.addEventListener('click', ()=>{
+    efect.classList.toggle('position_lozango');
+});
+ 
+
