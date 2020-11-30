4d0fe68a0514f329286e1b619613a29196fe03af
Ronaldo Mariano <ronaldowebdesing@hotmail.com>
2020-11-30 09:56:10 -0300

    update: inicio dos script

---
+++ script.js
@@ -0,0 +1,10 @@
+let efect = document.querySelector('.efect')
+let efect1 = document.querySelector('.efect_one')
+let efect2 = document.querySelector('.efect_two')
+let efect3 = document.querySelector('.efect_three')
+let efect4 = document.querySelector('.efect_four')
+
+efect1.addEventListener('click', ()=>{
+    efect.classList.toggle("cicle");
+});
+console.log(efect1)
