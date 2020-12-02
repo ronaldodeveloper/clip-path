d0956a4ffe09d9579e5b2c8eb9655e631db64837
Ronaldo Mariano <ronaldowebdesing@hotmail.com>
2020-12-02 09:09:29 -0300

    end: atualizacao final

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 
-let efect = document.querySelector('#efect')
 
+window.onload= ()=>{ //apos a pagina carregar execute
+
+let efect = document.querySelector('#efect')
 let efect1 = document.querySelector('.efect_one')
 let efect2 = document.querySelector('.efect_two')
 let efect3 = document.querySelector('.efect_three')
 let efect4 = document.querySelector('.efect_four')
 
-efect1.addEventListener('click', ()=>{ 
-    efect.classList.toggle('position_trapezio');
-});
-efect2.addEventListener('click', ()=>{
-    efect.classList.toggle('position_cicle');
-});
-efect3.addEventListener('click', ()=>{
-    efect.classList.toggle('position_pentagono');
-});
-efect4.addEventListener('click', ()=>{
-    efect.classList.toggle('position_lozango');
-});
- 
 
+ efect1.addEventListener('click', ()=>
+ { 
+    efect.classList.toggle('position_chevron');
+ });
+
+ efect2.addEventListener('click', ()=>
+ {
+    efect.classList.toggle('position_cicle');
+ });
+
+ efect3.addEventListener('click', ()=>
+ {
+    efect.classList.toggle('position_pentagono');
+ });
+
+ efect4.addEventListener('click', ()=>
+ {
+    efect.classList.toggle('position_lozango');
+ });
+}
